482181203a53247e29e2dbe82234e6fd36987bf5
faquefer <163359135+faquefer@users.noreply.github.com>
2024-03-14 15:43:30 +0000

    hola

--- juegos/cachipun.py
+++ juegos/cachipun.py
@@ -1,7 +1,32 @@
+import random
 def cachipun():
-    """
-    Esta función representa el juego de cachipun.
-    Debes pedir al usuario que elija piedra, papel o tijera, y luego comparar su elección con la de la computadora.
-    La computadora debe elegir una opción al azar.
-    """
+    print("ca")
+    print("chi")
+    print("pun")
+    eleccion = input()
+    mierda = random.randint(1,3)
+    if eleccion == "tijera":
+        if mierda == 1:
+            return str("tijera, empataste")
+        elif mierda == 2:
+            return str("piedra, perdiste")
+        elif mierda == 3:
+            return str("papel, ganaste")
+        
+    elif eleccion == str("piedra"):
+        if mierda == 1:
+            return str("tijera, ganaste")
+        elif mierda == 2:
+            return str("piedra, empataste")
+        elif mierda == 3:
+            return str("papel, perdiste")
+    
+    elif eleccion == str("papel"):
+        if mierda == 1:
+            return str("tijera, perdiste")
+        elif mierda == 2:
+            return str("piedra, ganaste")
+        elif mierda == 3:
+            return str("papel, empataste")
     pass
+print(cachipun())
