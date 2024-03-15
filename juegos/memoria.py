e2fcaf3daf638aff636385eec5e2476cc0ae8e3f
Fercrack2004 <163358870+Fercrack2004@users.noreply.github.com>
2024-03-15 14:43:30 +0000

    cuando to la vi

--- juegos/memoria.py
+++ juegos/memoria.py
@@ -1,3 +1,5 @@
+import random
+import time
 def memoria():
     """
     Esta función representa el juego de memoria.
@@ -5,4 +7,16 @@ def memoria():
     Luego, debes pedir al usuario que repita la secuencia.
     Se debe mostrar un mensaje si el usuario acierta o no.
     """
-    pass
+    numero=random.randint(100000000,1000000000)
+    print("HEEEEEEEY, mucho ojo, tienes 5 segundos para memorizar este numero, 😨😨😨: ",numero)
+    time.sleep(5)
+    print(f"🟨🟨🟨🟨⬛\n🟨🟨🟨🟨⬛\n🟨🟨🟨🟨🟨⬛\n🟨🟨🟨🟨🟨🟨⬛⬛\n🟨🟨⬛⬛⬛⬛⬜⬜⬛\n🟨🟨⬛⬜⬜⬛⬜⬜⬜⬛\n🟨⬛⬜⬜⬜⬜⬛⬜⬛⬜⬛\n🟨⬛⬜⬜⬜⬜⬛⬜⬜⬛\n🟨⬛⬜⬜⬛⬜⬜⬛⬛\n🟨🟨⬛⬜⬜⬜⬜⬛🟨⬛\n🟨🟨🟨⬛⬛⬛🟨🟨🟨⬛\n🟨🟨🟨🟨🟨🟨🟨⬛⬛⬛\n🟨🟨🟨🟨🟨⬛⬛⬛⬛⬛\n🟨🟨🟨🟨⬛⬛⬛🟫🟫⬛\n🟨🟨🟨⬛🟫🟫🟫🟫🟫⬛\n🟨🟨⬛🟫🟫🟫🟫🟫🟫🟫⬛\n🟨🟨⬛🟫⬛🟫🟫🟫🟫⬛\n🟨🟨⬛🟫⬛⬛⬛⬛⬛\n🟨🟨⬛🟫🟫🟫⬛\n🟨🟨🟨⬛🟫🟫 Cuando yo la Vi")
+    time.sleep(2)
+    print("escribe el numero exactamente igual al que viste antes")
+    lol=int(input())
+    if lol==numero:
+        print("HEEEEEEEEEEEEY, eres un genio, felicidades shinji")
+    else:
+        print("wuawua, que pena, perdiste 👎")
+    return
+memoria()
